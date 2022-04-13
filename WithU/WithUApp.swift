0528f29f8f55949d531890895cf367d9428d446d
seosh5704 <seosh5704@gmail.com>
2022-04-13 19:54:59 +0900

    Initial Commit

---
+++ WithU/WithUApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithUApp.swift
+//  WithU
+//
+//  Created by seosh on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WithUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
