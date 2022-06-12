3f7eb177b87ab5619425e073b311dac570789a9f
seosh5704 <seosh5704@gmail.com>
2022-06-12 21:52:37 +0900

    Create CustomActionSheet

--- WithU/Home/ChangeUserProfileView.swift
+++ WithU/Home/ChangeUserProfileView.swift
@@ -14,51 +14,102 @@ struct ChangeUserProfileView: View {
     @State var isShowingPopupview = false
     
     var body: some View {
-        VStack( spacing: 15) {
-            ZStack {
-                Color.gray
-                    .frame(width: 250, height: 250)
-                    .opacity(0.2)
-                Image(systemName: "plus")
-                    .resizable()
-                    .frame(width: 50, height: 50)
-                    .scaledToFit()
-                    .foregroundColor(.black)
+        ZStack {
+            VStack( spacing: 15) {
+                ZStack {
+                    Color.gray
+                        .frame(width: 250, height: 250)
+                        .opacity(0.2)
+                    Image(systemName: "plus")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .scaledToFit()
+                        .foregroundColor(.black)
+                }
+                .onTapGesture {
+                    isShowingPopupview.toggle()
+                }
+                VStack {
+                    TextField("애칭을 입력하세요.", text: $user.nickName )
+                        .frame(width: 250)
+                        .foregroundColor(.black)
+                    Divider()
+                        .frame(width: 250, height: 1)
+                        .background(Color.gray)
+                    Button(action: {
+                        setUserInfo(user: user)
+                        isShowingChangeUserPopup.toggle()
+                    }, label: {
+                        Text("확인")
+                        
+                            .fontWeight(.bold)
+                            .foregroundColor(.buttonForeground)
+                    })
+                    .frame(width: 250, height: 30)
+                    .background(Color.buttonBackground)
+                    .cornerRadius(12)
+                }
+                
             }
-            .onTapGesture {
-                isShowingPopupview.toggle()
-            }
-            VStack {
-                TextField("애칭을 입력하세요.", text: $user.nickName )
-                    .frame(width: 250)
-                    .foregroundColor(.black)
-                Divider()
-                    .frame(width: 250, height: 1)
-                    .background(Color.gray)
-                Button(action: {
-                    setUserInfo(user: user)
-                    isShowingChangeUserPopup.toggle()
-                }, label: {
-                    Text("확인")
+            .padding()
+            .background(Color.white)
+            .cornerRadius(12)
+        }
+        
+        VStack {
+            Spacer()
+            
+            CustomActionSheet(isShowingPopupview: $isShowingPopupview).offset(y: self.isShowingPopupview ? 0 : UIScreen.main.bounds.height)
+        }.background(self.isShowingPopupview ? Color.black.opacity(0.3) : Color.clear)
+            .edgesIgnoringSafeArea(.bottom)
+        
+        
+        
+        
+    }
+}
+
+//커스텀 액션시트
+struct CustomActionSheet: View {
+    @Binding var isShowingPopupview: Bool
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 15) {
+            HStack {
+                Button(action: {} ) {
+                    Image(systemName: "person.crop.rectangle")
+                    Text("앨범에서 선택하기")
                     
-                        .fontWeight(.bold)
-                        .foregroundColor(.buttonForeground)
-                })
-                .frame(width: 250, height: 30)
-                .background(Color.buttonBackground)
-                .cornerRadius(12)
+                }
+                .foregroundColor(Color.black)
+                Spacer()
             }
             
-        }
-        .padding()
-        .background(Color.white)
-        .cornerRadius(12)
-        .popup(isPresented: $isShowingPopupview, type: .default, position: .bottom, closeOnTap: false, closeOnTapOutside: true) {
-            Text("The popup")
-                .frame(width: 200, height: 60)
-                .background(Color(red: 0.85, green: 0.8, blue: 0.95))
-                .cornerRadius(30.0)
-        }
+            HStack {
+                Button(action: {} ) {
+                    Image(systemName: "camera")
+                    Text("카메라로 찍기")
+                    
+                }
+                .foregroundColor(Color.black)
+                Spacer()
+            }
+            HStack {
+                Button(action: {
+                    isShowingPopupview.toggle()
+                }) {
+                    Image(systemName: "clear")
+                    Text("취소")
+                    
+                }
+                .foregroundColor(Color.black)
+                Spacer()
+            }
+        }.padding(.bottom, (UIApplication.shared.windows.last?.safeAreaInsets.bottom)! + 10)
+            .padding(.horizontal)
+            .padding(.top, 20)
+            .background(Color.white)
+            .cornerRadius(20)
         
     }
 }
