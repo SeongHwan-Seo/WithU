a6326415d03243f56220cf1bfef31752eb372775
SeongHwan-Seo <seosh5704@gmail.com>
2023-06-02 12:57:56 +0900

    불피요한 코드 삭제

--- WithU/WithUApp.swift
+++ WithU/WithUApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import FirebaseCore
+import Kingfisher
 
 @main
 struct WithUApp: App {
@@ -24,6 +25,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        
         return true
     }
 }
